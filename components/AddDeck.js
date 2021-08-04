92c8d400be17e7387298c999d0fc09fba538fd28
Eric Jones <eric.web.email@gmail.com>
2021-08-04 08:17:34 -0400

    Added Form To AddDeck Component

--- components/AddDeck.js
+++ components/AddDeck.js
@@ -1,30 +1,77 @@
 import React, { Component } from "react";
-import {View, Text, StyleSheet} from "react-native";
+import {View, Text, StyleSheet, TextInput, TouchableOpacity} from "react-native";
+import TextButton from "./TextButton";
 import {getDecks} from "../utils/api";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
+  label: {
+    marginHorizontal: 12,
+  },
+  input: {
+    height: 60,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  btn: {
+    marginHorizontal: 12,
+    height: 60,
+    padding: 10,
+    borderRadius: 10,
+    backgroundColor: '#567',
+  },
+  btnText: {
+    color: '#FFF',
+    fontSize: 30
+  }
 });
 
 class AddDeck extends Component {
 
   state = {
-    decks: {}
+    deck_name: ""
   }
 
-  componentDidMount() {
+  onChangeText = (input) => {
+    this.setState({
+      deck_name: input
+    })
+  }
 
+  onPress = () => {
+    const { deck_name } = this.state;
+    console.log(deck_name);
+
+    // Store the value in Redux
+    
+    this.setState({
+      deck_name: ""
+    });
+
+    // Navigate Back To All Decks
+
+    // Store the value in AsyncStorage
   }
 
   render() {
+    const { deck_name } = this.state;
+
     return (
       <View style={styles.container}>
-        <Text>Add Deck View</Text>
+        <Text style={styles.label}>Name Your New Deck</Text>
+        <TextInput
+          onChangeText={this.onChangeText}
+          style={styles.input}
+          value={deck_name}
+        />
+        <TouchableOpacity style={styles.btn} onPress={ this.onPress }>
+          <Text style={styles.btnText}>Submit</Text>
+        </TouchableOpacity>
       </View>
     );
   }
---
+++ utils/colors.js
@@ -0,0 +1,2 @@
+// Add color palette here.
+// Each color should be constant and imported for consistent values.
